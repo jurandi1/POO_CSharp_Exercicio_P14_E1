a56cb099f414a6aabd83ab182842909c97b1c994
Jurandi Junior <jurandijr20@gmail.com>
2025-02-07 22:03:42 -0300

    Exercício concluído

---
+++ Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace POO_CSharp_Exercicio_P14_E1.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,57 @@
-﻿namespace POO_CSharp_Exercicio_P14_E1
+﻿using POO_CSharp_Exercicio_P14_E1.Entities;
+using System.Globalization;
+using System.IO;
+
+namespace POO_CSharp_Exercicio_P14_E1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            /*
+             * Fazer um programa para ler o caminho de um arquivo .csv
+            contendo os dados de itens vendidos. Cada item possui um
+            nome, preço unitário e quantidade, separados por vírgula. Você
+            deve gerar um novo arquivo chamado "summary.csv", localizado
+            em uma subpasta chamada "out" a partir da pasta original do
+            arquivo de origem, contendo apenas o nome e o valor total para
+            aquele item (preço unitário multiplicado pela quantidade),
+            conforme exemplo.
+             * */
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
